36aee1bef9ba93e37c32006752cd6a8d88ca2438
IrinaGnatiuk <50766668+IrinaGnatiuk@users.noreply.github.com>
2019-09-28 01:34:12 +0300

    Add files via upload

---
+++ HW_1.py
@@ -0,0 +1,29 @@
+class Playground:
+
+    def __init__(self, wight, height, cell_size):
+        self.wight = wight
+        self.height = height
+        self.cell_size = cell_size
+
+    def draw_grid(self):
+        pass
+
+
+class Ball:
+
+    def __init__(self, x, y, r, color):
+        self.x = x
+        self.y = y
+        self.r = r
+        self.color = color
+
+    def draw(self):
+        pass
+
+    def move_ball(self):
+        pass
+
+
+
+
+
